dd91de5d14465f5c8439812144928142b7e9dd0d
Alan <alan_hahn1995@outlook.com>
2024-09-20 18:07:27 -0300

    Criando as Rotas

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,27 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\{
+    AuthController,
+    UserController,
+    BarberController
+};
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+Route::post('/user', [AuthController::class, 'create']);
+
+Route::get('/user', [UserController::class, 'read']);
+Route::get('/user/favorites', [UserController::class, 'getFavorites']);
+Route::post('/user/favorite', [UserController::class, 'addFavorite']);
+Route::get('/user/appointments', [UserController::class, 'getAppointments']);
+
+Route::get('/barbers', [BarberController::class, 'list']);
+Route::get('/barbers/{id}', [BarberController::class, 'one']);
+Route::post('/barbers/{id}/appointment', [BarberController::class, 'setAppointments']);
+
+Route::get('/search', [BarberController::class, 'search']);
+
+
+
