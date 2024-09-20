f06679cb250804ca3c785c930a4269a0df24ca95
Alan <alan_hahn1995@outlook.com>
2024-09-20 18:48:29 -0300

    Criando usuário (1/2)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\{
+    Auth,
+    Validator
+};
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['create', 'login']]);
+    }
+
+    public function create(Request $request)
+    {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!$validator->fails()) {
+            $name = $request->input('name');
+            $email = $request->input('email');
+            $password = $request->input('password');
+    
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => password_hash($password, PASSWORD_DEFAULT)
+            ]);
+        } else {
+            $array['error'] = 'Dados incorretos';
+            return $array;
+        }
+
+        return $array;
+    }
 }
