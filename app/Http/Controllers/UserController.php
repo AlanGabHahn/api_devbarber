d61dd4067535f3f417f4ee3f1b289131d43cf561
Alan <alan_hahn1995@outlook.com>
2024-10-28 22:39:14 -0300

    Pegando informações do usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    private $logged;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+        $this->logged = auth()->user();
+    }
+
+    public function read()
+    {
+        $array = ['error' => ''];
+
+        $info = $this->logged;
+        $info['avatar'] = url('media/avatars/'.$info['avatar']);
+
+        $array['data'] = $info;
+
+        return $array;
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\{
 
 Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
 
+Route::get('/radom', [BarberController::class, 'createRandom']);
+
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/logout', [AuthController::class, 'logout']);
 Route::post('/auth/refresh', [AuthController::class, 'refresh']);
